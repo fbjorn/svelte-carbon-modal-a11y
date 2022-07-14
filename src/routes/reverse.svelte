d7dc5bb976a9f248d4e32688c2aa3b5d38b151ab
fbjorn <fbjorn@users.noreply.github.com>
2022-07-14 18:43:24 +0300

    Fix deploy

--- src/routes/reverse.svelte
+++ src/routes/reverse.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <script lang="ts">
   import "carbon-components-svelte/css/all.css";
   import { Dropdown, Modal, TextInput } from "carbon-components-svelte";
@@ -14,11 +18,6 @@
 >
   <h3>Try to hit TAB and iterate across inputs</h3>
   <h3>Then same with Shift-TAB</h3>
-  <div>
-    <a href="/">Dropdown first</a>
-    <br />
-    <a href="/reverse">Inputs first</a>
-  </div>
   <div class="content">
     <TextInput placeholder="text" name="text" labelText="Login" />
     <TextInput placeholder="password" type="password" labelText="Password" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 
 import preprocess from "svelte-preprocess";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,17 +9,13 @@ const config = {
 
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
       pages: "docs",
       assets: "docs",
-      fallback: null,
-      precompress: false,
     }),
-
-	prerender: { 
-		default: true,
-	}
+    paths: {
+      // change below to your repo name
+      base: "/svelte-carbon-modal-a11y",
+    },
   },
 };
 
